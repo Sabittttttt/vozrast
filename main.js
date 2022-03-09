40216baeeb5a643bb0bcc4f8e412fae6294aa4ae
Sabit <aysholpanmaulenova@gmail.com>
2022-03-09 22:30:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+ let name = prompt('введите своё имя');
+ let age = +prompt('введите год рождение');
+ let nowa = +prompt('введите нынешний год');
+
+ 
+let result = ( nowa - age )
+alert('Имя: ' +  name + ',' + ' ваш возраст ' + result );
+console.log( name + ',' + ' ваш возраст ' + result);
+
+ 
+
+
+
